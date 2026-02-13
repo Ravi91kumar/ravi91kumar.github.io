108a7f2a77fd679a0da9c05757daf13e7bfca1d5
Ravi Kumar <125450174+Ravi91kumar@users.noreply.github.com>
2026-02-13 15:27:20 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -102,14 +102,4 @@ window.updateProfile=function(){
  profileImg.src=url;
  localStorage.setItem("profileImg",url);
 };
-const toggleBtn = document.getElementById("theme-toggle");
 
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-        toggleBtn.textContent = "☀️";
-    } else {
-        toggleBtn.textContent = "🌙";
-    }
-});
